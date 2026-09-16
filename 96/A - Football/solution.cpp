327b5ed9a2ef4567604211459f68c8bb23183311
Md Touhidul Islam <mdtouhidulislamsojol@gmail.com>
2026-09-16 23:06:59 +0600

    Add Football [A] from Codeforces

---
+++ 96/A - Football/solution.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    string s;
+    cin >> s;
+    
+    if(s.find("0000000") != string::npos || s.find("1111111") != string::npos) {
+        cout << "YES" << endl;
+    } else {
+        cout << "NO" << endl;
+    }
+    
+    return 0;
+}
