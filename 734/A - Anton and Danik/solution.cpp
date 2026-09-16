ea498f40c970d8d667c1cd240371d6c75a3fdfa2
Md Touhidul Islam <mdtouhidulislamsojol@gmail.com>
2026-09-16 23:06:04 +0600

    Add Anton and Danik [A] from Codeforces

---
+++ 734/A - Anton and Danik/solution.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+    int a=0, d=0;
+    for(int i=0; i<s.size(); i++)
+    {
+        if(s[i] == 'A') a++;
+        else d++;
+    }
+    if(a>d) cout << "Anton";
+    else if(d>a) cout << "Danik";
+    else cout << "Friendship";
+    return 0;
+}
