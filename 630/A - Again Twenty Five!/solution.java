3283bf08955f9581e00490ce32626ec1e6a3176a
Md Touhidul Islam <mdtouhidulislamsojol@gmail.com>
2026-09-18 19:41:21 +0600

    Add Again Twenty Five! [A] from Codeforces

---
+++ 630/A - Again Twenty Five!/solution.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class AAgainTwentyFive
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        long a = Long.parseLong(input.nextLine());
+        System.out.println(25);
+        input.close();
+    }
+}
